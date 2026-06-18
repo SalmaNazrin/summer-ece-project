bee78ed1f1fbc453afd99f3879ec16cdf4851ee7
Salma Nazrin <salma07nazrin@gmail.com>
2026-06-18 22:15:29 +0530

    Add testbench for half adder module

---
+++ vlsi/testbench1.sv
@@ -0,0 +1,16 @@
+module testbench;
+    reg a, b;
+    wire sum, carry;
+    
+    half_adder uut(.a(a), .b(b), .sum(sum), .carry(carry));
+ 
+    initial begin
+        $dumpfile("dump.vcd");
+    $dumpvars(0, testbench);
+        $display("a b | sum carry");
+        a=0; b=0; #10; $display("%b %b |  %b    %b", a, b, sum, carry);
+        a=0; b=1; #10; $display("%b %b |  %b    %b", a, b, sum, carry);
+        a=1; b=0; #10; $display("%b %b |  %b    %b", a, b, sum, carry);
+        a=1; b=1; #10; $display("%b %b |  %b    %b", a, b, sum, carry);
+    end
+endmodule
