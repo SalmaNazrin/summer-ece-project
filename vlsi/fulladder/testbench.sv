980f242a50554ac1f5a7ffeb76aeb4c62ef79a07
Salma Nazrin <salma07nazrin@gmail.com>
2026-06-18 22:20:03 +0530

    Add testbench for full adder simulation
    This testbench simulates a full adder by testing all input combinations and displaying the results.

---
+++ vlsi/fulladder/testbench.sv
@@ -0,0 +1,20 @@
+module testbench;
+    reg a, b, cin;
+    wire sum, cout;
+    
+    full_adder uut(.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
+    
+    initial begin
+       $dumpfile("dump.vcd");
+       $dumpvars(0, testbench);
+        $display("a b cin | sum cout");
+        a=0;b=0;cin=0; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=0;b=0;cin=1; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=0;b=1;cin=0; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=0;b=1;cin=1; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=1;b=0;cin=0; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=1;b=0;cin=1; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=1;b=1;cin=0; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+        a=1;b=1;cin=1; #10; $display("%b %b  %b  |  %b    %b",a,b,cin,sum,cout);
+    end
+endmodule
